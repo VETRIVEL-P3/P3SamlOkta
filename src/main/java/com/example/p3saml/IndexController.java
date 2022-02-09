e3c3e6e64b1d543e89ca598907bc12d12a828d2d
VETRIVEL-P3 <98636637+VETRIVEL-P3@users.noreply.github.com>
2022-02-09 13:22:59 +0530

    SAML With Username Fetched

--- src/main/java/com/example/p3saml/IndexController.java
+++ src/main/java/com/example/p3saml/IndexController.java
@@ -1,7 +1,16 @@
 package com.example.p3saml;
 
+import org.apache.catalina.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import static org.springframework.security.core.context.SecurityContextHolder.getContext;
 
 @Controller
 public class IndexController {
@@ -10,4 +19,27 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @GetMapping(value = "/auth")
+    public String handleSamlAuth() {
+        Authentication auth = getContext().getAuthentication();
+        if (auth != null) {
+            return "redirect:/home";
+        }
+
+        else {
+            return "/";
+        }
+    }
+    @RequestMapping(value="/home",method = RequestMethod.GET)
+    public String home(ModelMap model) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        model.addAttribute("name", authentication.getPrincipal());
+        return "home";
+
+
+
+    }
+
 }
